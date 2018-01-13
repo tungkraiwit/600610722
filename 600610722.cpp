84fe2fc77d33ba66a462d64b6223ac28c0c98487
tungkraiwit <35339526+tungkraiwit@users.noreply.github.com>
2018-01-13 16:21:38 +0700

    modify readme.md

---
+++ 600610722.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Name 		:	Kraiwit Keyen \n";
+	cout<<"Student ID 	:	600610722 \n";
+	cout<<"Section 		:	002 \n";
+	cout<<"Age 			:	19 \n";
+	cout<<"Your programming skill (0-5)	:  	1 \n";
+	cout<<"Expected grade this course 	:  	A \n";
+	cout<<"Message to lecturers or TAs 	:  	I'm handsome ";
+	return 0;
+}
